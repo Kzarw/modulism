34ebf5f02f24dce847c389049a5b5e7d2382339a
Nikita Skornyakov <kzarw@mail.ru>
2024-06-22 21:28:25 +0300

    Solution #3: Gradle (HARD)

---
+++ tax/tax-api-impl/src/main/java/ru/tbank/javaconf/modulism/tax/entity/TaxRateDefault.java
@@ -0,0 +1,23 @@
+package ru.tbank.javaconf.modulism.tax.entity;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Table("tax_rate_defaults")
+public class TaxRateDefault {
+
+  @Id
+  private Integer year;
+
+  @NotNull
+  @Min(0)
+  private BigDecimal rate;
+}
